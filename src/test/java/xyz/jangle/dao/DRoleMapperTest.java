8bce6720fef27aa5a19c14ae191a4f8a6daf7d0d
bof-jangle <jangle@jangle.xyz>
2018-09-18 11:01:37 +0800

    mybati代码生成工具添加，角色表与角色相关的类添加。

---
+++ src/test/java/xyz/jangle/dao/DRoleMapperTest.java
@@ -0,0 +1,26 @@
+package xyz.jangle.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+
+import xyz.jangle.model.DRole;
+import xyz.jangle.test.utils.JUnitRunSupport;
+
+public class DRoleMapperTest extends JUnitRunSupport {
+	
+	@Autowired
+	private DRoleMapper dRoleMapper;
+
+	@Test
+	public void testInsert() {
+		DRole record =new DRole();
+		record.setRolName("2");
+		int insert = dRoleMapper.insert(record );
+		System.out.println(insert);
+		assertEquals(insert, 1);
+	}
+
+}
