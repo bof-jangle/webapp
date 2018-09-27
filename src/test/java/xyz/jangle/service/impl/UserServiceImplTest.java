016181621142a6044cdc36d127536ab00a7db88b
bof-jangle <jangle@jangle.xyz>
2018-09-27 16:26:23 +0800

    1

--- src/test/java/xyz/jangle/service/impl/UserServiceImplTest.java
+++ src/test/java/xyz/jangle/service/impl/UserServiceImplTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Rollback;
 
 import xyz.jangle.model.User;
 import xyz.jangle.service.UserService;
