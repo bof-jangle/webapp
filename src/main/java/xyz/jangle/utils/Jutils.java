81943229bb8a12c599310f67e98aa87aa7ade150
bof-jangle <jangle@jangle.xyz>
2020-01-13 10:29:45 +0800

    基于java8的lambda表达式的，线程安全的日期格式化工具方法。

--- src/main/java/xyz/jangle/utils/Jutils.java
+++ src/main/java/xyz/jangle/utils/Jutils.java
@@ -1,15 +1,25 @@
 package xyz.jangle.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+
+import javax.swing.text.DateFormatter;
 
 /**
  * @author jangle E-mail: jangle@jangle.xyz
  * @version 2019年1月21日 下午5:52:48 类说明
  */
 public class Jutils {
+
+	private static Map<String, ThreadLocal<DateFormatter>> dateLocalMap = new HashMap<>();
+	
+	// 线程安全的日期格式化
+//	private final static ThreadLocal<DateFormatter> formatter = ThreadLocal.withInitial(() -> new DateFormatter(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")));
+
 	/**
 	 * 参数大于0
 	 * 
@@ -49,25 +59,57 @@ public class Jutils {
 	public static boolean isEmpty(String arg) {
 		return !Jutils.isNotEmpty(arg);
 	}
-	
+
 	public static boolean isEmpty(List<?> list) {
 		return list == null || list.isEmpty();
 	}
-	
+
 	/**
 	 * 获取HashMap<String,Object>的对象
+	 * 
 	 * @return
 	 */
-	public static Map<String, Object> getHashMapSO(){
+	public static Map<String, Object> getHashMapSO() {
 		return new HashMap<String, Object>();
 	}
-	
+
 	/**
 	 * 获取新的UUID
+	 * 
 	 * @return
 	 */
 	public static String getUuid() {
 		return UUID.randomUUID().toString().replaceAll("-", "");
+	}
+
+	/**
+	 * 	日期格式转换
+	 * 	每种type类型一个ThreadLocal。
+	 * 	每个线程对应与该ThreadLocal中的独立实例（详见threadLocal.get()方法）
+	 * @param date 日期
+	 * @param type 字符串类型（例：yyyy年MM月dd日 HH:mm:ss.SSS ）
+	 * @return
+	 */
+	public static String datetimeFormatter(Date date, String type) {
+		if (date == null || type == null) {
+			return "";
+		}
+		ThreadLocal<DateFormatter> threadLocal = dateLocalMap.get(type);
+		if (threadLocal == null) {
+			threadLocal = ThreadLocal.withInitial(() -> new DateFormatter(new SimpleDateFormat(type)));
+			dateLocalMap.put(type, threadLocal);
+		}
+		return threadLocal.get().getFormat().format(date);
+	}
+
+	/**
+	 * 日期格式转换
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public static String datetimeFormatter(Date date) {
+		return datetimeFormatter(date, "yyyy-MM-dd HH:mm:ss");
 	}
 
 }
