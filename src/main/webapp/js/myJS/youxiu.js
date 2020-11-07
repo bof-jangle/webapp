63424b863cbbba163b784850d557a68b94ed7c74
bof-jangle <jangle@jangle.xyz>
2020-11-07 11:14:43 +0800

    记录优秀的实用的js函数。

---
+++ src/main/webapp/js/myJS/youxiu.js
@@ -0,0 +1,7 @@
+
+/** 为url添加键值对 */
+function addURLParam(url, name, value) {
+	url += (url.indexOf("?") == -1 ? "?" : "&");
+	url += encodeURIComponent(name) + "=" + encodeURIComponent(value);
+	return url;
+}
